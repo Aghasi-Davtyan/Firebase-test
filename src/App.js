5b2df0fb1e849686ab12f9138957564e6343774a
Aghasi Davtyan <aghasdavtyan@gmail.com>
2020-03-13 15:51:10 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
+import axios from './axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    name: [],
+  }
+
+  componentDidMount() {
+    axios.get('/person.json')
+      .then(response=>{
+        let names = []
+        for(let i in response.data){
+          names.push(response.data[i].firstName)
+          this.setState({name: names}) 
+        }
+        console.log(names)
+     }
+      )
+  }
+  // componentDidUpdate() {
+  //   axios.get('/person.json')
+  //     .then(response=>{
+  //       let names = []
+  //       for(let i in response.data){
+  //         names.push(response.data[i].firstName)
+  //         this.setState({name: names}) 
+  //       }
+  //       console.log(names)
+  //    }
+  //     )
+  // }
+
+  handlePost = () => {
+    let obj = {
+      firstName: 'Edo',
+      lastName: 'Sensai',
+      bills: 2430,
+    }
+
+    axios.post('/person.json',obj)
+    .then(response=>console.log(response))
+    .catch(error=>console.log(error))
+  }
+
+    handleGet = () => {
+      axios.get('/person.json')
+      .then(response=>{
+        console.log(response.data)
+     }
+      )
+      .catch(error=>console.log(error))
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <div className={'container'}>
+          <button onClick={this.handlePost}>Post</button>
+          <form className={'form'}>
+          <button onClick={this.handleGet}>Get</button>
+            <div>
+              <label>First Person</label>
+              <select>
+              {this.state.name.map(name=> {
+                return<option>{name}</option>
+              })}
+            </select>
+              <input type='number' />
+            </div>
+            <div>
+              <label>Second Person</label>
+              <select>
+              {this.state.name.map(name=> {
+                return<option>{name}</option>
+              })}
+            </select>
+            </div>
+            <button type='submit'>Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
